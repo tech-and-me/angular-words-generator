c6e22ca51839430984bbbc61c2b6556b9d2a195d
Pary <81phary@gmail.com>
2022-08-28 18:26:44 +1000

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import wordArray from '../utils/words'
+import countryArray from '../utils/countries'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-word-generator';
+  words:string = '';
+  country:string = '';
+  limit:number = 1;
+  randomNum:number=0;
+
+  handleSlideChange(newLimit:number){
+    this.limit = newLimit;
+  }
+
+  generateWords(){
+    this.country = ''
+    this.words = wordArray.slice(0,this.limit).join(' ');
+  }
+
+  generateRandomCountry(){
+    this.words = ''
+    this.randomNum=Math.floor(Math.random() * countryArray.length)
+    this.country = countryArray[this.randomNum];
+  }
+  
+  reset(){
+    this.words = ''
+    this.country = ''
+  }
 }
---
+++ src/utils/countries.ts
@@ -0,0 +1,14 @@
+// some random countries
+export default [
+  'Cambodia',
+  'Malaysia',
+  'Singapore',
+  'Australia',
+  'Vietnam',
+  'Thailland',
+  'New Zealand',
+  'France',
+  'Mexico',
+  'Canada',
+  'Italy'
+];
